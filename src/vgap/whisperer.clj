372c588f3064c52e9251f98a2f1890473ae8d40c
Eric Lavigne <lavigne.eric@gmail.com>
2016-04-18 05:03:34 -0700

    Include min/max-nu-rank in Whisperer export

--- src/vgap/whisperer.clj
+++ src/vgap/whisperer.clj
@@ -1,8 +1,9 @@
 (ns vgap.whisperer
-  (:require [vgap.storage :as storage]
+  (:require [vgap.nu-api :as nu]
+            [vgap.storage :as storage]
             [vgap.util :as util]))
 
-(defn convert-game [game]
+(defn convert-game [game nu-game]
   (let [final-turn (apply max (keys (:planets game)))]
     (sorted-map
       :id (:id game)
@@ -23,17 +24,22 @@
                               (filter #(<= % 30)
                                       (keys (:relations game)))))
       :host (:host game)
+      :min-nu-rank (:min-nu-rank nu-game)
+      :max-nu-rank (:max-nu-rank nu-game)
     )))
 
 (defn export []
-  (let [game-ids (storage/fetch-game-summary-ids-from-s3)]
+  (let [game-ids (storage/fetch-game-summary-ids-from-s3)
+        nu-games-by-id (group-by :game-id (nu/fetch-rated-games))]
     (doseq [game-id game-ids]
       (let [game (read-string (storage/fetch-game-summary-from-s3 game-id))
-            converted (convert-game game)
+            nu-game (first (nu-games-by-id game-id))
+            converted (convert-game game nu-game)
             final-turn (:final-turn converted)
             final-turn-date (:end-date converted)
             final-turn-year (aget (.split (or final-turn-date "unknown") "-") 0)
             file-name (str "whisperer/" final-turn-year "/game-" game-id ".edn")]
+        (when (:min-nu-rank converted) (println (str "Found game with min rank: " file-name)))
         (clojure.java.io/make-parents file-name)
         (spit file-name (util/pprint-edn converted))))))
 
