bc21a3ace7345a0dc99197610e3a4773785c9101
Eric Lavigne <lavigne.eric@gmail.com>
2016-04-18 04:47:50 -0700

    Include min-nu-rank and max-nu-rank in nu/fetch-rated-games

--- src/vgap/nu_api.clj
+++ src/vgap/nu_api.clj
@@ -10,6 +10,15 @@
   "Parse Nu-style datetime like 9/23/2014 6:35:52 PM and return date (without time)"
   (.toLocalDate (timef/parse datetime-format datetime-string)))
 
+(defn convert-levelid-to-rank-description [levelid]
+  (let [conversion-table {1 "Midshipman"      2 "Ensign"      3 "Sub-Lieutenant"
+                          4 "Lieutenant"      5 "Lieutenant Commander"
+                          6 "Commander"       7 "Captain"     8 "Commodore"
+                          10 "Vice Admiral"  15 "Emperor"}]
+    (cond (contains? #{nil 0} levelid) nil
+          (contains? conversion-table levelid) (conversion-table levelid)
+          :else (throw (Exception. (str "Unrecognized Nu rank " levelid))))))
+
 (defn fetch-rated-games [] ; 5 seconds, basic info about all games, should filter by :ended property
   (let [api-games (json/read-str
                           (:body (client/get "http://api.planets.nu/games/list?status=2,3"
@@ -19,6 +28,10 @@
                            create-date (parse-datetime-as-date (g "datecreated"))]
                        (merge {:name (g "name") :description (g "description")
                                :created create-date
+                               :min-nu-rank-id (g "requiredlevelid")
+                               :max-nu-rank-id (g "maxlevelid")
+                               :min-nu-rank (convert-levelid-to-rank-description (g "requiredlevelid"))
+                               :max-nu-rank (convert-levelid-to-rank-description (g "maxlevelid"))
                                :game-id (g "id")}
                               (if (and end-date (not (.isBefore end-date create-date)))
                                   {:ended end-date}
